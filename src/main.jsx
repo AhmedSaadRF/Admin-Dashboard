32a11a6d521be5061ef971f412d8d131a601828f
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-07-12 15:20:42 +0300

    Upload Project

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import './index.css'
+import App from './App';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc.  */}
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
