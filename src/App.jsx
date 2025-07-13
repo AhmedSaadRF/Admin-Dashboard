52b51b6de7d59c19b2ee50db50656b4bbaf4da9a
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-07-13 10:29:00 +0300

    Upload Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import TopBar from './components/TopBar';
-import SideBar from './components/SideBar';
-import { styled } from '@mui/material/styles';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import TopBar from "./components/TopBar";
+import SideBar from "./components/SideBar";
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
+import { getDesignTokens } from "./theme";
 
-
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
-
-
 export default function MiniDrawer() {
+  const [mode, setMode] = React.useState(
+    localStorage.getItem("currentmode")
+      ? localStorage.getItem("currentmode")
+      : "dark"
+  );
+
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -30,17 +33,23 @@ export default function MiniDrawer() {
     setOpen(false);
   };
 
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <TopBar open={open} handleDrawerOpen={handleDrawerOpen} />
-      <SideBar open={open} handleDrawerClose={handleDrawerClose} />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        <Typography sx={{ marginBottom: 2 }}>
-          Lorem ipsum
-        </Typography>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <TopBar
+          open={open}
+          handleDrawerOpen={handleDrawerOpen}
+          setMode={setMode}
+        />
+        <SideBar open={open} handleDrawerClose={handleDrawerClose} />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Typography sx={{ marginBottom: 2 }}>Ahmed Saad Alrefaey</Typography>
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
