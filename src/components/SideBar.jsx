b803e5ede5c994a34ad3f495e3e9b82686d7a6fa
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-07-13 11:12:14 +0300

    Upload Project

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -12,6 +12,18 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { styled, useTheme } from '@mui/material/styles';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import { Avatar, Typography } from '@mui/material';
 
 const drawerWidth = 240;
 
@@ -71,6 +83,24 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
+const Array1 = [
+  { "text": "Dashboard", "icon": <HomeOutlinedIcon />, "path": "/" },
+  { "text": "Manage Team", "icon": <PeopleOutlinedIcon />, "path": "/team" },
+  { "text": "Contacts Information", "icon": <ContactsOutlinedIcon />, "path": "/contacts" },
+  { "text": "Invoices Balances", "icon": <ReceiptOutlinedIcon />, "path": "/invoices" },
+];
+const Array2 = [
+  { "text": "Profile Form", "icon": <PersonOutlinedIcon />, "path": "/form" },
+  { "text": "Calendar", "icon": <CalendarTodayOutlinedIcon />, "path": "/calendar" },
+  { "text": "FAQ", "icon": <HelpOutlineOutlinedIcon />, "path": "/faq" },
+];
+const Array3 = [
+  { "text": "Bar Chart", "icon": <BarChartOutlinedIcon />, "path": "/bar" },
+  { "text": "Pie Chart", "icon": <PieChartOutlineOutlinedIcon />, "path": "/pie" },
+  { "text": "Line Chart", "icon": <TimelineOutlinedIcon />, "path": "/line" },
+  { "text": "Geography Chart", "icon": <MapOutlinedIcon />, "path": "/geography" },
+];
+
 export default function SideBar({ open, handleDrawerClose }) {
   const theme = useTheme();
 
@@ -82,9 +112,13 @@ export default function SideBar({ open, handleDrawerClose }) {
         </IconButton>
       </DrawerHeader>
       <Divider />
+      <Avatar sx={{ mx: 'auto', width: 88, height: 88, my: 1, border: '2px solid grey' }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Typography align='center' variant="body1">Ahmed Saad</Typography>
+      <Typography align='center' variant="body1">Admin</Typography>
+      <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+        {Array1.map((item) => (
+          <ListItem key={item.path} disablePadding sx={{ display: 'block' }}>
             <ListItemButton
               sx={[
                 {
@@ -115,10 +149,10 @@ export default function SideBar({ open, handleDrawerClose }) {
                     },
                 ]}
               >
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {item.icon}
               </ListItemIcon>
               <ListItemText
-                primary={text}
+                primary={item.text}
                 sx={[
                   open
                     ? {
@@ -135,8 +169,8 @@ export default function SideBar({ open, handleDrawerClose }) {
       </List>
       <Divider />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+        {Array2.map((item) => (
+          <ListItem key={item.path} disablePadding sx={{ display: 'block' }}>
             <ListItemButton
               sx={[
                 {
@@ -167,10 +201,62 @@ export default function SideBar({ open, handleDrawerClose }) {
                     },
                 ]}
               >
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {item.icon}
               </ListItemIcon>
               <ListItemText
-                primary={text}
+                primary={item.text}
+                sx={[
+                  open
+                    ? {
+                      opacity: 1,
+                    }
+                    : {
+                      opacity: 0,
+                    },
+                ]}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {Array3.map((item) => (
+          <ListItem key={item.path} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={[
+                {
+                  minHeight: 48,
+                  px: 2.5,
+                },
+                open
+                  ? {
+                    justifyContent: 'initial',
+                  }
+                  : {
+                    justifyContent: 'center',
+                  },
+              ]}
+            >
+              <ListItemIcon
+                sx={[
+                  {
+                    minWidth: 0,
+                    justifyContent: 'center',
+                  },
+                  open
+                    ? {
+                      mr: 3,
+                    }
+                    : {
+                      mr: 'auto',
+                    },
+                ]}
+              >
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={item.text}
                 sx={[
                   open
                     ? {
