9c52da5ffa8fb3110c77974f3a3b256e50c30472
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-07-13 11:47:01 +0300

    Upload Project

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,37 +1,35 @@
-import React from 'react'
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import MuiDrawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { styled, useTheme } from '@mui/material/styles';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
-import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
-import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
-import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
-import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
-import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
-import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
-import { Avatar, Typography } from '@mui/material';
+import React from "react";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import MuiDrawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { styled, useTheme } from "@mui/material/styles";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { Avatar, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const drawerWidth = 240;
 
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
@@ -39,87 +37,118 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    variants: [
-      {
-        props: ({ open }) => open,
-        style: {
-          ...openedMixin(theme),
-          '& .MuiDrawer-paper': openedMixin(theme),
-        },
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        ...openedMixin(theme),
+        "& .MuiDrawer-paper": openedMixin(theme),
       },
-      {
-        props: ({ open }) => !open,
-        style: {
-          ...closedMixin(theme),
-          '& .MuiDrawer-paper': closedMixin(theme),
-        },
+    },
+    {
+      props: ({ open }) => !open,
+      style: {
+        ...closedMixin(theme),
+        "& .MuiDrawer-paper": closedMixin(theme),
       },
-    ],
-  }),
-);
+    },
+  ],
+}));
 
 const Array1 = [
-  { "text": "Dashboard", "icon": <HomeOutlinedIcon />, "path": "/" },
-  { "text": "Manage Team", "icon": <PeopleOutlinedIcon />, "path": "/team" },
-  { "text": "Contacts Information", "icon": <ContactsOutlinedIcon />, "path": "/contacts" },
-  { "text": "Invoices Balances", "icon": <ReceiptOutlinedIcon />, "path": "/invoices" },
+  { text: "Dashboard", icon: <HomeOutlinedIcon />, path: "/" },
+  { text: "Manage Team", icon: <PeopleOutlinedIcon />, path: "/team" },
+  {
+    text: "Contacts Information",
+    icon: <ContactsOutlinedIcon />,
+    path: "/contacts",
+  },
+  {
+    text: "Invoices Balances",
+    icon: <ReceiptOutlinedIcon />,
+    path: "/invoices",
+  },
 ];
 const Array2 = [
-  { "text": "Profile Form", "icon": <PersonOutlinedIcon />, "path": "/form" },
-  { "text": "Calendar", "icon": <CalendarTodayOutlinedIcon />, "path": "/calendar" },
-  { "text": "FAQ", "icon": <HelpOutlineOutlinedIcon />, "path": "/faq" },
+  { text: "Profile Form", icon: <PersonOutlinedIcon />, path: "/form" },
+  { text: "Calendar", icon: <CalendarTodayOutlinedIcon />, path: "/calendar" },
+  { text: "FAQ", icon: <HelpOutlineOutlinedIcon />, path: "/faq" },
 ];
 const Array3 = [
-  { "text": "Bar Chart", "icon": <BarChartOutlinedIcon />, "path": "/bar" },
-  { "text": "Pie Chart", "icon": <PieChartOutlineOutlinedIcon />, "path": "/pie" },
-  { "text": "Line Chart", "icon": <TimelineOutlinedIcon />, "path": "/line" },
-  { "text": "Geography Chart", "icon": <MapOutlinedIcon />, "path": "/geography" },
+  { text: "Bar Chart", icon: <BarChartOutlinedIcon />, path: "/bar" },
+  { text: "Pie Chart", icon: <PieChartOutlineOutlinedIcon />, path: "/pie" },
+  { text: "Line Chart", icon: <TimelineOutlinedIcon />, path: "/line" },
+  { text: "Geography Chart", icon: <MapOutlinedIcon />, path: "/geography" },
 ];
 
 export default function SideBar({ open, handleDrawerClose }) {
   const theme = useTheme();
+  const navigate = useNavigate();
 
   return (
     <Drawer variant="permanent" open={open}>
       <DrawerHeader>
         <IconButton onClick={handleDrawerClose}>
-          {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          {theme.direction === "rtl" ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
         </IconButton>
       </DrawerHeader>
       <Divider />
-      <Avatar sx={{ mx: 'auto', width: 88, height: 88, my: 1, border: '2px solid grey' }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-      <Typography align='center' variant="body1">Ahmed Saad</Typography>
-      <Typography align='center' variant="body1">Admin</Typography>
+      <Avatar
+        sx={{
+          mx: "auto",
+          width: open ? 88 : 44,
+          height: open ? 88 : 44,
+          my: 1,
+          border: "2px solid grey",
+          transition: "0.25s",
+        }}
+        alt="Remy Sharp"
+        src="src\Assets\my-img.jpg"
+      />
+      <Typography align="center" sx={{ fontSize: open ? 17 : 0, transition: "0.25s" }}>
+        Ahmed Saad
+      </Typography>
+      <Typography align="center" sx={{ fontSize: open ? 15 : 0, transition: "0.25s", color: theme.palette.info.main }}>
+        Admin
+      </Typography>
       <Divider />
       <List>
         {Array1.map((item) => (
-          <ListItem key={item.path} disablePadding sx={{ display: 'block' }}>
+          <ListItem key={item.path} disablePadding sx={{ display: "block" }}>
             <ListItemButton
+              onClick={() => {
+                navigate(item.path);
+              }}
               sx={[
                 {
                   minHeight: 48,
@@ -127,10 +156,10 @@ export default function SideBar({ open, handleDrawerClose }) {
                 },
                 open
                   ? {
-                    justifyContent: 'initial',
+                    justifyContent: "initial",
                   }
                   : {
-                    justifyContent: 'center',
+                    justifyContent: "center",
                   },
               ]}
             >
@@ -138,14 +167,14 @@ export default function SideBar({ open, handleDrawerClose }) {
                 sx={[
                   {
                     minWidth: 0,
-                    justifyContent: 'center',
+                    justifyContent: "center",
                   },
                   open
                     ? {
                       mr: 3,
                     }
                     : {
-                      mr: 'auto',
+                      mr: "auto",
                     },
                 ]}
               >
@@ -170,8 +199,11 @@ export default function SideBar({ open, handleDrawerClose }) {
       <Divider />
       <List>
         {Array2.map((item) => (
-          <ListItem key={item.path} disablePadding sx={{ display: 'block' }}>
+          <ListItem key={item.path} disablePadding sx={{ display: "block" }}>
             <ListItemButton
+              onClick={() => {
+                navigate(item.path);
+              }}
               sx={[
                 {
                   minHeight: 48,
@@ -179,10 +211,10 @@ export default function SideBar({ open, handleDrawerClose }) {
                 },
                 open
                   ? {
-                    justifyContent: 'initial',
+                    justifyContent: "initial",
                   }
                   : {
-                    justifyContent: 'center',
+                    justifyContent: "center",
                   },
               ]}
             >
@@ -190,14 +222,14 @@ export default function SideBar({ open, handleDrawerClose }) {
                 sx={[
                   {
                     minWidth: 0,
-                    justifyContent: 'center',
+                    justifyContent: "center",
                   },
                   open
                     ? {
                       mr: 3,
                     }
                     : {
-                      mr: 'auto',
+                      mr: "auto",
                     },
                 ]}
               >
@@ -222,8 +254,11 @@ export default function SideBar({ open, handleDrawerClose }) {
       <Divider />
       <List>
         {Array3.map((item) => (
-          <ListItem key={item.path} disablePadding sx={{ display: 'block' }}>
+          <ListItem key={item.path} disablePadding sx={{ display: "block" }}>
             <ListItemButton
+              onClick={() => {
+                navigate(item.path);
+              }}
               sx={[
                 {
                   minHeight: 48,
@@ -231,10 +266,10 @@ export default function SideBar({ open, handleDrawerClose }) {
                 },
                 open
                   ? {
-                    justifyContent: 'initial',
+                    justifyContent: "initial",
                   }
                   : {
-                    justifyContent: 'center',
+                    justifyContent: "center",
                   },
               ]}
             >
@@ -242,14 +277,14 @@ export default function SideBar({ open, handleDrawerClose }) {
                 sx={[
                   {
                     minWidth: 0,
-                    justifyContent: 'center',
+                    justifyContent: "center",
                   },
                   open
                     ? {
                       mr: 3,
                     }
                     : {
-                      mr: 'auto',
+                      mr: "auto",
                     },
                 ]}
               >
@@ -272,6 +307,5 @@ export default function SideBar({ open, handleDrawerClose }) {
         ))}
       </List>
     </Drawer>
-  )
+  );
 }
-
